596e45ebbacebf4a300c5177cbf58e71aa0155e6
Hiroyuki Anai <pirosikick@gmail.com>
2017-02-06 21:15:47 +0900

    Rename to readModule.js

---
+++ test/readModule.js
@@ -0,0 +1,16 @@
+const test = require('ava');
+const readModule = require('../lib/readModule');
+
+function* testReadModule(t, id, expected) {
+  const actual = yield readModule(id);
+  t.deepEqual(actual, expected);
+}
+
+test('./fixtures/a.json', testReadModule, 'fixtures/a.json', {
+  $ref: 'module:./b.json',
+});
+
+test('./fixtures/d', testReadModule, './fixtures/d', {
+  type: 'string',
+  example: 'd.js',
+});
